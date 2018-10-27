6c1650f9724b761fd6e93d104a8fd624e3866620
student <student@imac-08.local>
2018-10-27 10:37:08 +0800

    Initial Commit

---
+++ 6.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  6.1
+//
+//  Created by student on 2018/10/27.
+//  Copyright © 2018年 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
